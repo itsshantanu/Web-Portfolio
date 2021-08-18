9c310c4fe00abeed28de25cab5215f977b8b4f74
Shantanu Bharadwaj <theshantanubharadwaj@gmail.com>
2021-08-18 16:13:39 +0530

    top-section-first with animation

---
+++ animate.js
@@ -0,0 +1,30 @@
+// $(document).ready(function(){
+//      //setInterval(function (){
+//     //     $(".slide-wrap span").each(function(i, obj) {
+//     //         // console.log(obj);
+//     //         // console.log(i);
+//     //         if($(obj).hasClass("is-visible")){
+//     //             // console.log(obj);
+//     //             setTimeout(function (){
+//     //                 $(obj).removeClass("is-visble").addClass("is-hidden");
+    
+//     //             }, 5000);
+//     //             $(obj).next("span").removeClass("is-hidden").addClass("is-visible");
+                 
+//     //         }    
+//     //         else if($(i=1).hasClass("is-hidden")){ 
+//     //              $(this).removeClass("is-hidden").addClass("is-visible");
+//     //             // console.log("does not has class");
+//     //         }
+            
+//     //     });
+//     //   //}, 300);   
+
+//     $(".slide-wrap span").each(function(i, obj) {
+//         setTimeout(function(){
+//             $(obj).toggleClass('text-slider');
+//         },5000);
+        
+//     });
+// });
+
